b1969b47acbd35d76252d39e887239e342b7879d
Lenar <listok195@gmail.com>
2025-01-11 13:57:50 +0300

    Скрипт интеграции вынесен компонента в модуль компонента, также туда перенесено подключение стилей

--- progress/script.js
+++ progress/script.js
@@ -1,26 +1,7 @@
-export default class Progress {
-  constructor(rootElement) {
-    this.root = rootElement;
-    this.valueInput = this.root.querySelector("[data-value]");
-    this.animatedCheckbox = this.root.querySelector("[data-animated]");
-    this.hiddenCheckbox = this.root.querySelector("[data-hidden]");
-    this.roundElement = this.root.querySelector(".round");
-
-    // Установка начальных значений
-    this.setProgress(this.valueInput.value);
-    this.setAnimated(this.animatedCheckbox.checked);
-    this.setVisibility(this.hiddenCheckbox.checked);
-
-    // Привязка обработчиков событий
-    this.valueInput.addEventListener("input", (e) =>
-      this.setProgress(e.target.value),
-    );
-    this.animatedCheckbox.addEventListener("input", (e) =>
-      this.setAnimated(e.target.checked),
-    );
-    this.hiddenCheckbox.addEventListener("input", (e) =>
-      this.setVisibility(e.target.checked),
-    );
+export default class Progress extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
   }
 
   setProgress(value) {
@@ -34,4 +15,43 @@ export default class Progress {
   setAnimated(isAnimated) {
     this.roundElement.classList.toggle("animated", isAnimated);
   }
+
+  connectedCallback() {
+    const link = document.createElement("link");
+    link.setAttribute("rel", "stylesheet");
+    link.setAttribute("href", "/progress/style.css");
+    this.shadowRoot.appendChild(link);
+
+    fetch("/progress/index.html")
+      .then((response) => response.text())
+      .then((html) => {
+        const template = document.createElement("template");
+        template.innerHTML = html;
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+        this.initComponent();
+      });
+  }
+
+  initComponent() {
+    this.valueInput = this.shadowRoot.querySelector("[data-value]");
+    this.animatedCheckbox = this.shadowRoot.querySelector("[data-animated]");
+    this.hiddenCheckbox = this.shadowRoot.querySelector("[data-hidden]");
+    this.roundElement = this.shadowRoot.querySelector(".round");
+
+    this.setProgress(this.valueInput.value);
+    this.setAnimated(this.animatedCheckbox.checked);
+    this.setVisibility(this.hiddenCheckbox.checked);
+
+    this.valueInput.addEventListener("input", (e) =>
+      this.setProgress(e.target.value),
+    );
+    this.animatedCheckbox.addEventListener("input", (e) =>
+      this.setAnimated(e.target.checked),
+    );
+    this.hiddenCheckbox.addEventListener("input", (e) =>
+      this.setVisibility(e.target.checked),
+    );
+  }
 }
+
+customElements.define("progress-block", Progress);
